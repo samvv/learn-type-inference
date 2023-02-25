56e4599a624868c6ccb06a90d7aae4cade8efe99
Sam Vervaeck <samvv@pm.me>
2023-02-25 17:12:34 +0100

    Add a small interactive REPL to tutorial 01

--- tutorials/01-simple-hindley-milner/app/Main.hs
+++ tutorials/01-simple-hindley-milner/app/Main.hs
@@ -1,5 +1,50 @@
 module Main where
 
+import Control.Monad.IO.Class (liftIO)
+import Data.List (isPrefixOf)
+import Control.Monad.State
+import System.Console.Repline
+import qualified Data.Text as T
+
+import Language.Toy.Compiler
+import Language.Toy.Frontend (fParse, fInfer)
+import Language.Toy.Parser (pExpr)
+import qualified Language.Toy.TypeEnv as TE
+import Language.Toy.Infer (inferExpr)
+
+data ReplState = RS { count :: Int, env :: TE.TypeEnv }
+
+type Repl a = HaskelineT (StateT ReplState IO) a
+
+ini :: Repl ()
+ini = lift $ liftIO $ putStrLn "Welcome!\nPress Ctrl+D to exit."
+
+output = lift . liftIO . putStrLn
+
+cmd :: String -> Repl ()
+cmd input = do s <- get
+               lift $ put $ s { count = count s + 1 }
+               let fname = "#<repl:" ++ show (count s) ++ ">"
+               case fParse fname pExpr $ T.pack input of
+                  CompileFailure ds -> output $ "parse error: " <> show ds
+                  CompileSuccess ds expr -> case fInfer (env s) expr of
+                    CompileFailure ds -> output $ "type-checking error: " <> show ds
+                    CompileSuccess ds scm -> output $ show scm
+
+final :: Repl ExitDecision
+final
+  = do lift $ liftIO $ putStrLn "Goodbye!"
+       return Exit
+
+completer :: Monad m => WordCompleter m
+completer n = do
+  let names = ["let", "if"]
+  return $ filter (isPrefixOf n) names
+
+initState :: ReplState
+initState = RS { count = 1, env = TE.empty }
+
 main :: IO ()
 main
-  = do putStrLn "Hello, world!"
+  = void $ runStateT (evalRepl (const . pure $ "> ") cmd [] (Just ':') (Just "paste") (Word completer) ini final) initState
+
--- tutorials/01-simple-hindley-milner/src/Language/Toy/Frontend.hs
+++ tutorials/01-simple-hindley-milner/src/Language/Toy/Frontend.hs
@@ -1,9 +1,15 @@
 module Language.Toy.Frontend where
 
-import qualified Data.Text as T
 import Text.Megaparsec (runParser)
-import Language.Toy.Parser
+import Control.Ev.Eff (runEff)
+import Control.Ev.Util (exceptEither)
+import qualified Data.Text as T
+
 import Language.Toy.Compiler
+import Language.Toy.AST
+import Language.Toy.Parser
+import Language.Toy.Infer (inferExpr, Scheme)
+import qualified Language.Toy.TypeEnv as TE
 
 fParse :: FilePath -> Parser a -> T.Text -> CompileResult a
 fParse fname p input
@@ -11,3 +17,7 @@ fParse fname p input
       Left errs -> CompileFailure [(ParseDiagnostics errs)]
       Right x -> CompileSuccess [] x
 
+fInfer :: TE.TypeEnv -> Expr -> CompileResult Scheme
+fInfer env x = case runEff $ exceptEither $ inferExpr env x of
+  Left d -> CompileFailure [d]
+  Right scm -> CompileSuccess [] scm
